4f63556d1e3ca3196d1631c234adfaf6c517bcbd
liaoworking <liaoworking@gmail.com>
2018-01-15 18:28:26 +0800

    changed the frame of clearButton🤔

--- GHConsole/GHConsole/GHConsole.m
+++ GHConsole/GHConsole/GHConsole.m
@@ -69,7 +69,7 @@ typedef void (^readTextBlock)(void);
 
 - (void)configClearBtn{
     
-    _clearBtn = [[UIButton alloc]initWithFrame:CGRectMake(self.view.bounds.size.width - 100, 20, 60, 30)];
+    _clearBtn = [[UIButton alloc]initWithFrame:CGRectMake(self.view.bounds.size.width - 80, 20, 60, 30)];
     [_clearBtn addTarget:self action:@selector(clearText) forControlEvents:UIControlEventTouchUpInside];
     [_clearBtn setTitle:@"clear" forState:UIControlStateNormal];
     [_clearBtn setTitleColor:[UIColor colorWithRed:0/255.0 green:212/255.0 blue:59/255.0 alpha:1] forState:UIControlStateNormal];
